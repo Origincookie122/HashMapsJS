b2691543a7c858bf5492720493cc8e2f84fbe9a2
Origincookie <carson@origin-development.net>
2023-11-15 11:22:32 -0500

    New chaining system to help prevent collisions

--- src/HashMaps/hashmaps.js
+++ src/HashMaps/hashmaps.js
@@ -1,45 +1,50 @@
 class HashMap {
-    constructor() {
+    constructor(length) {
+        if(!length) return console.error('Please provide a length of the table')
         this.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        this.table = [];
+        this.table = new Array(length).fill(null).map(() => []); // Initialize an array of empty arrays
+        this.length = length
     }
+
     async hash(string) {
         if (!string) {
-            console.error('Please provide a string')
+            console.error('Please provide a string');
+            return null;
         }
 
         return new Promise((resolve, reject) => {
             var characters = string.split("");
-            var k = 0
+            var k = 0;
+
             for (var i = 0; i < characters.length; i++) {
                 var character = characters[i];
                 var index = this.alphabet.indexOf(character);
 
                 if (index !== -1) {
-                    k = k + index + 1
+                    k = k + index + 1;
                 } else {
-                    reject(`'${character}' not found in the alphabet.`)
+                    reject(`'${character}' not found in the alphabet.`);
                 }
             }
-            resolve(k);
-        })
+            resolve(k % this.length); // Ensure the index is within the range of the table length
+        });
     }
 
     async insert(v) {
-        //if(!k) return console.error('Provide a key') // Will use later
-        if (!v) return console.error('Provide a value')
+        if (!v) {
+            console.error('Provide a value');
+            return null;
+        }
 
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             try {
-                (async () => {
-                    const hashedString = await this.hash(v);
-                    this.table[hashedString] = v;
-                    resolve(hashedString);
-                })();
+                const hashedString = await this.hash(v);
+                this.table[hashedString].push(v);
+                resolve(hashedString);
             } catch (error) {
-                reject(error)
+                reject(error);
             }
-        })
+        });
     }
 }
 
