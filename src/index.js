bf05a02f6c40422a5e77363197bd7f830e5b076a
Origincookie <carson@origin-development.net>
2023-11-15 11:22:54 -0500

    Pretty much the same usage as last time

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const HashMap = require('./HashMaps/hashmaps');
 
 var array = [];
 var string = "hello"
-var hashmap = new HashMap();
+var hashmap = new HashMap(10);
 
 
 (async () => {
     try {
       const hashedString = await hashmap.hash(string);
       await hashmap.insert(string)
-      console.log(hashmap.table[hashedString]);
+      console.log(hashmap.table);
     } catch (error) {
       console.error(error);
     }
-  })();
+})();
 
